76f2948a38041820fa6edc714b525a1e74aadc56
thomastuts <thomas@thomastuts.com>
2016-07-26 22:18:41 +0200

    Add additional tests for new exercises

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,37 @@
 describe('Intro to Mocha', function() {
+  describe('theTruth()', function() {
+    it('should return true', function() {
+      expect(theTruth()).toEqual(true);
+    });
+  });
+
+  describe('sayHey()', function() {
+    it('should return the correct greeting', function() {
+      expect(sayHey()).toEqual('Hey!');
+    });
+  });
+
+  describe('sayHeyFriend()', function() {
+    it('should return a greeting using the given name', function() {
+      expect(sayHeyFriend('Johnny')).toEqual('Hey, Johnny!');
+    });
+  });
+
   describe('favoriteIceCream()', function() {
-    it('returns "I love ${flavor}."', function() {
-      expect(favoriteIceCream("mint chocolate chip")).toEqual("I love mint chocolate chip.");
+    it('should return the correct sentence when passed an icecream flavor', function() {
+      expect(favoriteIceCream('mint chocolate chip')).toEqual('I love mint chocolate chip');
     });
   });
 
   describe('shouting()', function() {
-    it('returns the given string in all caps', function() {
-      expect(shouting("i love javascript soooooo much")).toEqual('I LOVE JAVASCRIPT SOOOOOO MUCH');
+    it('should return the given string in all caps', function() {
+      expect(shouting('i love javascript soooooo much')).toEqual('I LOVE JAVASCRIPT SOOOOOO MUCH');
     });
   });
 
   describe('roundDown()', function() {
-    it('rounds the given number down', function() {
+    it('should return the given number, rounded down', function() {
       expect(roundDown(5.99)).toEqual(5);
-    });
-  });
-
-  describe('theTruth()', function() {
-    it('returns true', function() {
-      expect(theTruth()).toEqual(true);
     });
   });
 });
