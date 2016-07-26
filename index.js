616ae2ad0ab479663f7d3989ae9a4613b534e7e6
Charles Pletcher <cpletcher2@gmail.com>
2016-07-26 11:23:13 -0400

    Add solution

---
+++ index.js
@@ -0,0 +1,15 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}.`
+}
+
+function shouting(s) {
+  return s.toUpperCase()
+}
+
+function roundDown(n) {
+  return Math.floor(n)
+}
+
+function theTruth() {
+  return true
+}
