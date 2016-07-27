9363022be7a414308501467aab5cedfcf587bb13
thomastuts <thomas@thomastuts.com>
2016-07-27 09:01:35 +0200

    Remove old test file; port tests to new file

--- test/index-test.js
+++ test/index-test.js
@@ -1,36 +1,36 @@
-describe('Intro to Mocha', function() {
-  describe('theTruth()', function() {
-    it('should return true', function() {
+describe('Intro to Mocha', function () {
+  describe('theTruth()', function () {
+    it('should return true', function () {
       expect(theTruth()).toEqual(true);
     });
   });
 
-  describe('sayHey()', function() {
-    it('should return the correct greeting', function() {
+  describe('sayHey()', function () {
+    it('should return the string "Hey!"', function () {
       expect(sayHey()).toEqual('Hey!');
     });
   });
 
-  describe('sayHeyFriend()', function() {
-    it('should return a greeting using the given name', function() {
-      expect(sayHeyFriend('Johnny')).toEqual('Hey, Johnny!');
+  describe('sayHeyFriend(name)', function () {
+    it("should return a greeting with the given name", function () {
+      expect(sayHeyFriend('Kristin')).toBe('Hey, Kristin!');
     });
   });
 
-  describe('favoriteIceCream()', function() {
-    it('should return the correct sentence when passed an icecream flavor', function() {
+  describe('favoriteIceCream()', function () {
+    it('should return the correct sentence when passed an icecream flavor', function () {
       expect(favoriteIceCream('mint chocolate chip')).toEqual('I love mint chocolate chip');
     });
   });
 
-  describe('shouting()', function() {
-    it('should return the given string in all caps', function() {
+  describe('shouting()', function () {
+    it('should return a string in all caps', function () {
       expect(shouting('i love javascript soooooo much')).toEqual('I LOVE JAVASCRIPT SOOOOOO MUCH');
     });
   });
 
-  describe('roundDown()', function() {
-    it('should return the given number, rounded down', function() {
+  describe('roundDown()', function () {
+    it('should correctly floor the number', function () {
       expect(roundDown(5.99)).toEqual(5);
     });
   });
