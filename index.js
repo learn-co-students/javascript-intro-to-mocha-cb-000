b7df82428f35afc067b66917689564da1e72db69
Uzor Jon-Ubabuco <uubabukoh@gmail.com>
2017-05-19 17:56:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey (){
+  return 'Hey!'
+}
+
+function sayHeyFriend (friend){
+  return `Hey, ${friend}!`
+}
+
+function favoriteIceCream (icecream){
+  return `I love ${icecream}`
+}
+
+function shouting(text){
+  return text.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
