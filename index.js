fd9faa219aae8b0184db03ea8c71500881ad7137
joel <joelbitar@protonmail.com>
2017-06-11 14:03:20 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}	
+
+function theTruth() {
+	return true
+}
+
+function sayHey() {
+	return 'Hey!'
+}
+function sayHeyFriend(argument) {
+	return `Hey, ${argument}!`
+}
+
+function shouting(string) {
+	return string.toUpperCase()
+}
+
+function roundDown(float) {
+	return Math.floor(float)
+}
