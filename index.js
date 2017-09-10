ee0d4e23901879a99bbe23d9706717a2d708150a
john vascsinec <pensteeler@gmail.com>
2017-09-10 20:43:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(friendName) {
+  return `Hey, ${friendName}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting( lowerString ) {
+  return lowerString.toUpperCase();
+}
+
+function roundDown( input ) {
+  return Math.floor(input);
+}
