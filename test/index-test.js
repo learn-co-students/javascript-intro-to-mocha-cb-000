873be16842d52ac8b898d3efa9b06c39f3d1baea
Matt Hough <matt@hough.tv>
2018-01-19 08:06:42 +1000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -35,3 +35,36 @@ describe('Intro to Mocha', function () {
     });
   });
 });
+
+global.expect = require('expect');
+
+const jsdom = require('jsdom');
+const path = require('path');
+
+before(function(done) {
+  const src = path.resolve(__dirname, '..', 'index.js');
+  const babelResult = require('babel-core').transformFileSync(src, {
+    presets: ['es2015']
+  });
+  const html = path.resolve(__dirname, '..', 'index.html');
+
+  jsdom.env(html, [], { src: babelResult.code }, (err, window) => {
+    if (err) {
+      return done(err);
+    }
+
+    Object.keys(window).forEach(key => {
+      global[key] = window[key];
+    });
+
+    return done();
+  });
+});
+
+describe('favoriteIceCream', () => {
+  it('should return the correct sentence when passed an icecream flavor', () => {
+    const result = favoriteIceCream('mint chocolate chip');
+    const expectedResult = 'I love mint chocolate chip';
+    expect(result).toBe(expectedResult);
+  });
+});
