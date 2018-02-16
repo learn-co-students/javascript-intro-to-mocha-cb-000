9d8020a393659c58b556d05e30c60c68252e2451
Michael Anderson <michanders@msn.com>
2018-02-16 22:43:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function favoriteIceCream(flavor) {
+  return "I love "+flavor
+}
+
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return "Hey, " + name + "!"
+}
+
+function shouting(string){
+  return string.toUpperCase()
+}
+
+function roundDown(num){
+  return Math.floor(num)
+}
