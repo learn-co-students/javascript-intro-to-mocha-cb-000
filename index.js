9ca519fac07bdf93e6c0ad5eb69344c6eedc837f
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-19 21:51:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return `Hey!`
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(shout) {
+  return shout.toUpperCase()
+}
+
+function roundDown(n) {
+  return Math.floor(n)
+}
