76b3265c9abfd1f69050fd948a79bcbe8cc46ae7
Vishal Sharma <vsh1819@gmail.com>
2018-08-13 06:35:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth () {
+  return true
+}
+
+function sayHey () {
+  return 'Hey!'
+}
+
+function sayHeyFriend (name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream (name) {
+  return `I love ${name}`
+}
+
+function shouting (name) {
+  return name.toUpperCase()
+}
+
+function roundDown (number) {
+  return Math.floor(number)
+}
