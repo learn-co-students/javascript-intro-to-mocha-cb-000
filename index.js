1e64d80411e5f2a738ea69db37b570d067dbc41d
Quinn Dizon <qdizon@gmail.com>
2018-09-28 12:30:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+const theTruth = () => {
+  return true;
+}
+
+const sayHey = () => {
+  return "Hey!"
+}
+
+const sayHeyFriend = (name) => {
+  return `Hey, ${name}!`
+}
+
+const favoriteIceCream = (flavor) => {
+  return `I love ${flavor}`
+}
+
+const shouting = (str) => {
+  return str.toUpperCase()
+}
+
+const roundDown = (num) => {
+  return Math.floor(num)
+}
