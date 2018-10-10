56273a18340ab81aeaa5ec446714e78bf9801a8a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-10 10:34:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function favoriteIceCream(flavour) {
+  return "I love " + flavour
+}
+
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return "Hey, " + name + "!"
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(number) {
+  return Math.floor(number)
+}
