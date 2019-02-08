dd2481f6041a741e7c22ce86ebd38f11c56cde2a
dapawn <dapawn@gmail.com>
2019-02-08 07:57:45 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+
