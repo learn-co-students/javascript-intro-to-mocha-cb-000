68ed105fb5c8743dad0b84588e621eba8911700d
Fer Hernandez <fh.1111@gmail.com>
2019-02-11 02:05:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
