71cb8cc14003140b2806c63057252c46869ba756
marysue <mary@marylark.com>
2019-02-15 13:09:04 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function favoriteIceCream(flavor) {
+    return  `I love ${flavor}`
+}
+
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+    return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+    return `Hey, ${name}!`;
+}
+
+function shouting(inStr) {
+    return inStr.toUpperCase();
+}
+
+function roundDown (inNbr) {
+    return Math.floor(inNbr);
+}
