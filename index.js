090652614e907ef45c31654fb81b0a4557b97a87
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-11-30 19:38:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(name_of_icecream) {
+  return `I love ${name_of_icecream}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
